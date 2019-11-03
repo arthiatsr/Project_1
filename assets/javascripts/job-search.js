ef05937c5bc484eec4a96b91a593146e713be91d
arthiatsr <arthiatsr@users.noreply.github.com>
2019-11-03 12:21:46 -0600

    added the code to populate firebase database

--- assets/javascripts/job-search.js
+++ assets/javascripts/job-search.js
@@ -1,41 +1,89 @@
 console.log("hi");
+
 $(document).ready(function() {
+
+var searchResultArray = [];
+
+//console.log(btnValue,jobRole,jobLocation);
+var queryURL="";
+var i;
+
+function gitajaxcall() {
+
+    //ajax call
+    
+    $.ajax({
+        url:queryURL,
+        method:"GET"
+
+    })
+    .then(function(response){
+
+        console.log(response);
+        var database = firebase.database();
+        for(i = 0; i < response.length; i++){
+        searchResultArray.push({"company" : response[i].company, "location" : response[i].location, "description" : response[i].description, "url" : response[i].url});       
+        
+        console.log(searchResultArray); 
+        
+        
+        }   
+
+    });    
+}
+
+function gitfirebasecall() {
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAbwLZoW9or1wG4Jn7lHesJ5kXC8ZDGooo",
+    authDomain: "jobsearch-69d6f.firebaseapp.com",
+    databaseURL: "https://jobsearch-69d6f.firebaseio.com",
+    projectId: "jobsearch-69d6f",
+    storageBucket: "jobsearch-69d6f.appspot.com",
+    messagingSenderId: "889128462674",
+    appId: "1:889128462674:web:834f4203377e955596b28b",
+    measurementId: "G-RMJJ3D382J"
+    };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+console.log(searchResultArray);
+var database = firebase.database();
+
+for (i=0; i< searchResultArray.length; i++){
+       jobname = searchResultArray[i].company;
+       console.log(jobname);
+       location = searchResultArray[i].location;
+       console.log(location);
+       description=searchResultArray[i].description;
+       url=searchResultArray[i].url;
+       
+       console.log("jobname: " +  jobname, "loc: " + location, "desc: " + description, "url: " + url);
+       // Code for "Setting values in the database"
+       database.ref().set({
+           name: jobname,
+           location: location,
+           description: description,
+           url: url
+       });
+    }
+}
 
 $(".btn").on("click",function(e){
     e.preventDefault();
     var btnValue= $(this).text();
     var jobRole = $("#role-input").val().trim();
 
-    var jobLocation = $("#location-input").val().trim();
-    console.log(btnValue,jobRole,jobLocation);
-    var queryURL="";
-    var searchResultArray = [];
-            
-    // function populateDatabase(){
-
-    //     var jobname;
-    //     var location;
-    //     var description;
-    //     var url;
-               
+var jobLocation = $("#location-input").val().trim();
         
-    //     database.ref().set({
-    
-    //         name: jobname,
-    //         location: location,
-    //         description: description,
-    //         url:url
-    
-    // });
-    // }
-    
     if(btnValue ==="GitHub"){
         if((jobRole!=="") && (jobLocation!=="")){
             queryURL = "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description="+jobRole+"&location="+jobLocation;
 
-                console.log("i ma in gitHub" + queryURL + jobRole + jobLocation);
-                
-                //populateDatabase();
+                console.log("i ma in gitHub" + queryURL + jobRole + jobLocation);                
+                console.log(queryURL);  
+                gitajaxcall();
+                gitfirebasecall();
                                 
         } else if((jobLocation=="") && (jobRole!=="")){
 
@@ -67,63 +115,16 @@ $(".btn").on("click",function(e){
 
         }
     } 
-    console.log(queryURL);  
     
-    $.ajax({
-        url:queryURL,
-        method:"GET"
-
-
-    })
-    .then(function(response){
-
-        console.log(response);
-       for(var i = 0; i < response.length; i++){
-        searchResultArray.push({"company" : response[i].company, "location" : response.location, "description" : response[i].description, "url" : response[i].url});
-        
-        
-        console.log(searchResultArray);
-        }
-
-    });
     
-//     const firebaseConfig = {
-//         apiKey: "AIzaSyA4eD_m0QUq1FuBqzt7Y1G3B8oMgXU6Zhw",
-//         authDomain: "project2-4152e.firebaseapp.com",
-//         databaseURL: "https://project2-4152e.firebaseio.com",
-//         projectId: "project2-4152e",
-//         storageBucket: "project2-4152e.appspot.com",
-//         messagingSenderId: "606489782220",
-//         appId: "1:606489782220:web:407a7d004079a2c7541f69",
-//         measurementId: "G-STSB19EERQ"
-//       };
-    
-//       firebase.initializeApp(firebaseConfig );
-    
-//       var database = firebase.database();
+      
 
-//       var jobname;
-//       var location;
-//       var description;
-//       var url;
-
+    
  });
 
-
-
-    
-
-    // database.ref().on("value", function(snapshot) {
-
-    //  //   var jobname = response.database.
-
-    // }, function(errorObject) {
-    //     console.log("The read failed: " + errorObject.code);
-    // });
-
-
-// Your web app's Firebase configuration
-
-
-
+ 
 });
+
+        
+   
+        
